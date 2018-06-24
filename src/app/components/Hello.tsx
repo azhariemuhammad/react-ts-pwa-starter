e09a2b5f25ecd937c2cc43a9476cc544588a5ea6
azharie <azharie@staff.gramedia.com>
2018-06-24 21:12:05 +0700

    initialize

---
+++ src/app/components/Hello.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+interface IProps {
+  compiler: string,
+  framework: string,
+  bundler: string
+}
+
+export class Hello extends React.Component<IProps, {}> {
+  render() {
+    return( 
+      <div>
+        <h1>This is a {this.props.framework} application using {this.props.compiler} with {this.props.bundler}
+        </h1>
+        <div>
+          foo
+        </div>
+        </div>
+    )
+  }
+}
